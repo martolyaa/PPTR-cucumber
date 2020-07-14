bfe0d214525d0609d8de449eb6073f170fe317e7
martolyaa <37909601+martolyaa@users.noreply.github.com>
2020-07-14 21:23:20 +0300

    Test example

---
+++ report.js
@@ -0,0 +1,16 @@
+const reporter = require("cucumber-html-reporter");
+
+const options = {
+    // themes
+    // 1) bootstrap
+    // 2) hierarchy
+    // 3) foundation
+    // 4) simple
+    theme: "bootstrap",
+    jsonFile: "cucumber_report.json",
+    output: "cucumber_report.html",
+    reportSuiteAsScenarios: true, 
+    launchReport: false
+};
+
+reporter.generate(options);
